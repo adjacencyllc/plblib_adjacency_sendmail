5435d922c38dc53e37c538b888509e8a9aa7a58e
Brian Jackson <bjackson@adjtech.com>
2026-01-09 12:54:02 -0600

    added release notes to release.bat

--- release.bat
+++ release.bat
@@ -2,5 +2,12 @@
 copy bin\lib_adjacency_sendmail.plc .\release\ /y
 copy src\lib_adjacency_sendmail.inc .\release\ /y
 copy doc\lib_adjacency_sendmail.md .\release\ /y
+
+echo Release files copied. To create a release on github:
+echo git checkout main (or master)
+echo git pull
+echo git tag vx.y.z
+echo git push origin vx.y.z
+
 echo Contents of /release directory
 dir .\release\*
